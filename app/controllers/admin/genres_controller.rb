5a6bdfd2d75374f48dadb1a492363dd96b224589
Natumesiki <soioopqgle@gmail.com>
2023-01-16 11:05:29 +0000

    migrate作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,25 @@
 class Admin::GenresController < ApplicationController
   def edit
+  #   @sweet = Sweet.new
+  # @admin =current_user.id
+  # @genre = Genre.find(params[:id])
+  # if @genre.user == current_user
+  # render "edit"
+  # else
+  # redirect_to admin_index_path
   end
 
   def index
+  #   @genre = Genre.new
+  # @admin = current_user.id
+  # @genres = Genres.all
   end
-end
+
+ def create
+  #   genre = Genre.new(genre_params)
+  #   gemre.save
+
+  #   redirect_to admin_genres_path(genre.id)
+  end
+
+ end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,44 @@
 class Admin::ItemsController < ApplicationController
   def edit
+      # @sweet = Sweet.find(params[:id])
   end
 
   def index
+    # @sweet = Sweet.new
+    # @sweets = Sweets.all
   end
 
   def new
+    @new = Item.new
   end
 
   def show
+    # @sweet = Sweet.find(params[:id])
   end
+
+ def update
+    # sweet = Sweet.find(params[:id])
+    # sweet.update(sweet_params)
+    # redirect_to admin_items_path(sweet.id)
+ end
+
+def create
+    #   @sweet = Sweet.new(sweet_params)
+    #   @sweet.user_id = current_user.id
+    #   if @sweet.save
+    # redirect_to admin_index_path(@sweet.id), notice: "You have created book successfully."
+    #   else
+    # @user = current_user
+    # @sweets = Sweet.all
+    # render 'admin/index'
+      # end
+end
+
+
+ private
+
+  def sweet_params
+    # params.require(:items).permit(:price,:genre,:title,:body,:user_id)
+  end
+
 end
