ae2b2f65dbf3d7cc8e7fb90f87be8e487f8d01b0
Natumesiki <soioopqgle@gmail.com>
2023-01-16 12:33:08 +0000

    migrate作成

--- db/migrate/20230116105727_create_admin_items.rb
+++ db/migrate/20230116105727_create_admin_items.rb
@@ -1,6 +1,11 @@
 class CreateAdminItems < ActiveRecord::Migration[6.1]
   def change
     create_table :admin_items do |t|
+      t.string :title
+      t.text :body
+      t.integer :price
+      t.string :genre
+      t.integer :user_id
 
       t.timestamps
     end
